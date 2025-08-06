2e102d4def838ab366c1e75c3322196807a8da2f
FIDEL GOLDSMAN <49002222@ort>
2025-08-06 10:37:25 -0300

    Cambios 1

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TP06___Repaso___ToDo_List.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
